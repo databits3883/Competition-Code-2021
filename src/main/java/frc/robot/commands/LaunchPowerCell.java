9b0e96f32e0f61eab7c8e13a6e8a5a84f845b81c
3883DriverStation <3883programming@gmail.com>
2020-02-08 15:07:58 -0600

    added functionality to power cell launcher

--- src/main/java/frc/robot/commands/LaunchPowerCell.java
+++ src/main/java/frc/robot/commands/LaunchPowerCell.java
@@ -29,16 +29,18 @@ public class LaunchPowerCell extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    m_UpperStagingBelt.runBelt();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_UpperStagingBelt.stopBelt();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return !m_UpperStagingBelt.isBallPresent();
   }
 }
