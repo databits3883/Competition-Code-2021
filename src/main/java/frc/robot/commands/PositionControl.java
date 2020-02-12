463126f907f3f6b3cab2fa88fde9e57b42f0af24
3883WillNotCharge <3883programming@gmail.com>
2020-02-11 19:56:55 -0600

    Finished Positional control command. still needs to add to robot container.

--- src/main/java/frc/robot/commands/PositionControl.java
+++ src/main/java/frc/robot/commands/PositionControl.java
@@ -7,7 +7,6 @@
 
 package frc.robot.commands;
 
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -15,9 +14,11 @@ import frc.robot.subsystems.ControlPanelSpinner;
 import frc.robot.subsystems.ControlPanelSpinner.WheelColor;
 
 public class PositionControl extends CommandBase {
-    ControlPanelSpinner.WheelColor targetUnderSensor;
     private final ControlPanelSpinner m_controlPanelSpinner;
-    boolean spinLeft; 
+    ControlPanelSpinner.WheelColor targetUnderSensor;
+    int targetindex;
+    int currentindex;
+    boolean[][] directions = {{true, false, false, true},{true, true, false, false},{false,true,true,false},{false,false,true,true}};
 
   /**
    * Creates a new PositionControl.
@@ -26,37 +27,62 @@ public class PositionControl extends CommandBase {
     m_controlPanelSpinner = controlPanelSpinner;
     addRequirements(controlPanelSpinner);
   }
+
     // Use addRequirements() here to declare subsystem dependencies.
   
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
     switch(DriverStation.getInstance().getGameSpecificMessage().charAt(0)){
       case 'R':
         targetUnderSensor = WheelColor.kBlue;
+        targetindex =2;
         break;
       case 'Y':
         targetUnderSensor = WheelColor.kGreen;
+        targetindex =3;
         break;
       case 'B':
         targetUnderSensor = WheelColor.kRed;
+        targetindex =0;
         break;
       case 'G':
         targetUnderSensor = WheelColor.kYellow;
+        targetindex =1;
         break;
       default:
+        
         break;
     }
-    
+    switch (m_controlPanelSpinner.getColor()){
+      case kBlue:
+      currentindex=0;
+      break;
+      case kGreen:
+      currentindex=1;
+      break;
+      case kRed:
+      currentindex=2;
+      break;
+      case kYellow:
+      currentindex=3;
+    break;
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    if ( m_controlPanelSpinner.getEngaged()){
+      if (directions [targetindex][currentindex]){
+      m_controlPanelSpinner.spinClockwise();
+      }
+      else{
+      m_controlPanelSpinner.spinCounterclockwise();
+      }
   }
-
+  }
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
@@ -65,6 +91,12 @@ public class PositionControl extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (m_controlPanelSpinner.getColor()==targetUnderSensor);
+    if(m_controlPanelSpinner.getColor()==targetUnderSensor){
+      m_controlPanelSpinner.stopSpin();
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
