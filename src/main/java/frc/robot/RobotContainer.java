599a43a3088c5e755c004e69217524db6fc5d722
3883WillNotCharge <3883programming@gmail.com>
2020-02-29 10:40:44 -0600

    added disabled safety command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import frc.robot.util.SupplierButton;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -223,5 +224,17 @@ public class RobotContainer {
 
   public Command getInitCommand(){
     return m_initCommand;
+  }
+
+  public Command getDisabledCommand(){
+    return new ParallelCommandGroup(
+      new RunCommand(m_turretRotator::setCurrentPosition, m_turretRotator),
+      new RunCommand(()->m_launcher.setSpeed(0), m_launcher),
+      new RunCommand(m_hood::setCurrentPosition, m_hood)
+    ){@Override
+      public boolean runsWhenDisabled(){
+        return true;
+      }
+    };
   }
 }
