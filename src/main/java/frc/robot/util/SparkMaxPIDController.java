31d2ef49ad42cad6a957864080267c1851d6f4f1
alex <49699649+alex-vanasse@users.noreply.github.com>
2020-10-21 17:46:13 -0500

    removed shuffleboard interaction from pid controller

--- src/main/java/frc/robot/util/SparkMaxPIDController.java
+++ src/main/java/frc/robot/util/SparkMaxPIDController.java
@@ -7,21 +7,15 @@
 
 package frc.robot.util;
 
-import java.util.function.DoubleConsumer;
 
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 
-import edu.wpi.first.wpilibj.Sendable;
-import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
-import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
-
 /**
  * Controller for PID operations with the Spark Max motor controller
  */
-public class SparkMaxPIDController implements Sendable{
-    static int instances =0;
+public class SparkMaxPIDController{
     CANSparkMax m_motor;
     CANPIDController m_controller;
     double m_setpoint;
@@ -35,7 +29,6 @@ public class SparkMaxPIDController implements Sendable{
     public SparkMaxPIDController(CANSparkMax sparkMax){
         m_motor = sparkMax;
         m_controller = m_motor.getPIDController();
-        SendableRegistry.addLW(this,"Spark Max PID Controller",instances++);
     }
     //gain setters & getters
     public void setP(double P){
@@ -82,16 +75,5 @@ public class SparkMaxPIDController implements Sendable{
     public void setSetpoint(double newSetpoint){
         m_setpoint=newSetpoint;
         m_controller.setReference(m_setpoint, m_controlType);
-    }
-
-
-    public void initSendable(SendableBuilder builder){
-        builder.setSmartDashboardType("PIDController");
-        builder.addDoubleProperty("p", this::getP, this::setP);
-        builder.addDoubleProperty("i", this::getI, this::setI);
-        builder.addDoubleProperty("d", this::getD, this::setD);
-        builder.addDoubleProperty("f", this::getFF, this::setFF);
-
-        builder.addDoubleProperty("Setpoint", this::getSetpoint, this::setSetpoint);
     }
 }
