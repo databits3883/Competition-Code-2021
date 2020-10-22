91d423c20ac4e9e14f3bbed6acdaaee3b6c59f25
alex <49699649+alex-vanasse@users.noreply.github.com>
2020-10-21 19:24:35 -0500

    added PIDTuningParameters object for organization

---
+++ src/main/java/frc/robot/util/PIDTuningParameters.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.util;
+
+/**
+ * Data Object for hodling PID tuning values
+ */
+public class PIDTuningParameters {
+    public double p;
+    public double i;
+    public double d;
+    public double ff;
+
+    public PIDTuningParameters(double initialP, double initialI, double initialD,double initialFF){
+        p = initialP;
+        i=initialI;
+        d=initialD;
+        ff=initialFF;
+    }
+    public PIDTuningParameters(double initialP, double initialI, double initialD){
+        this(initialP,initialI,initialD,0);
+    }
+}
