c4c1f8991f3a38ebabb3bc1d83f8c8ffc9b228cb
alex <49699649+alex-vanasse@users.noreply.github.com>
2020-10-22 12:50:09 -0500

    added setpoint and process variable display to shuffleboard

--- src/main/java/frc/robot/subsystems/SparkMaxPIDSubsystem.java
+++ src/main/java/frc/robot/subsystems/SparkMaxPIDSubsystem.java
@@ -8,10 +8,16 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.ControlType;
+
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.PIDTuningParameters;
 import frc.robot.util.SparkMaxPIDController;
@@ -20,11 +26,28 @@ public abstract class SparkMaxPIDSubsystem extends SubsystemBase {
   /**
    * Creates a new SparkMaxPIDSubsystem.
    */
-  public SparkMaxPIDSubsystem(String name, CANSparkMax mainMotor, ControlType controlType, PIDTuningParameters tuning) {
+  public SparkMaxPIDSubsystem(String name, CANSparkMax mainMotor, ControlType controlType, PIDTuningParameters tuning, double conversionFactor) {
+    //set up conversion factor and accessor for the process variable
+    DoubleSupplier processVariable;
+    CANEncoder encoder = mainMotor.getEncoder();
+    switch (controlType){
+      case kPosition: encoder.setPositionConversionFactor(conversionFactor);
+        processVariable = encoder::getPosition;
+        break;
+      case kVelocity: encoder.setVelocityConversionFactor(conversionFactor);
+        processVariable = encoder::getVelocity;
+        break;
+      default: throw new IllegalArgumentException("SparkMaxPIDSubsystem doesn't yet support control type "+controlType.toString());
+    }
+
     SparkMaxPIDController mainController =new SparkMaxPIDController(mainMotor,controlType,tuning);
 
-    ShuffleboardLayout tuningLayout = Shuffleboard.getTab(name).getLayout("tuning");
+    ShuffleboardTab tab = Shuffleboard.getTab(name);
+    ShuffleboardLayout tuningLayout = tab.getLayout("tuning");
     mainController.addTuningToShuffleboard(tuningLayout);
+    //add a graph with the setpoint and the current value
+    tab.addDoubleArray("Process Variable vs Setpoint", ()->(new double[] {processVariable.getAsDouble(),mainController.getSetpoint()}))
+      .withWidget(BuiltInWidgets.kGraph);
   }
 
   @Override
