0a78bfe8f26fa3cebb135d95965dd39768787352
Alex <49699649+alex-vanasse@users.noreply.github.com>
2020-11-17 18:46:35 -0600

    converted TurretLauncher to SparkMaxPIDSubsystem

--- src/main/java/frc/robot/subsystems/TurretLauncher.java
+++ src/main/java/frc/robot/subsystems/TurretLauncher.java
@@ -7,79 +7,50 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
+import com.revrobotics.CANDigitalInput.LimitSwitchPolarity;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.Constants;
-import frc.robot.util.SetpointVelocityLimiter;
+import frc.robot.util.PIDTuningParameters;
 import frc.robot.Variables;
 
-public class TurretLauncher extends SubsystemBase {
-  private final CANSparkMax leader = new CANSparkMax(Constants.launcherLeaderChannel, MotorType.kBrushless);
-  private final CANSparkMax follower = new CANSparkMax(Constants.launcherFollowerChannel, MotorType.kBrushless);
+public class TurretLauncher extends ProfiledSparkMaxPIDSubsystem{
+  private static final double conversionFactor = (4.0/12.0*Math.PI)*(1.0/60.0);
 
-  private final CANEncoder encoder = new CANEncoder(leader);
-  private final CANPIDController controller = new CANPIDController(leader);
-
-  private double p,i,d,ff,speed;
-  private NetworkTableEntry pEntry,iEntry,dEntry,ffEntry,speedEntry;
-  NetworkTableEntry limitedentry;
-
-  public SetpointVelocityLimiter m_setpointLimiter = new SetpointVelocityLimiter(25);
+  private final CANSparkMax m_follower;
   /**
    * Creates a new Launcher.
    */
   public TurretLauncher() {
-    follower.follow(leader,true);
-    leader.setIdleMode(IdleMode.kCoast);
-    follower.setIdleMode(IdleMode.kCoast);
-    encoder.setVelocityConversionFactor((4.0/12.0*Math.PI)*(1.0/60.0));
+    super("Turret Launcher",new CANSparkMax(Constants.launcherLeaderChannel, MotorType.kBrushless),ControlType.kVelocity,new PIDTuningParameters(0.03,0,0.1,0.005),conversionFactor,
+    -200,0,
+    25,LimitSwitchPolarity.kNormallyOpen);
+    m_follower = new CANSparkMax(Constants.launcherFollowerChannel,MotorType.kBrushless);
 
-    pEntry = Shuffleboard.getTab("LaunchingTuning").add("portional",p).getEntry();
-    iEntry = Shuffleboard.getTab("LaunchingTuning").add("integral",i).getEntry();
-    dEntry = Shuffleboard.getTab("LaunchingTuning").add("derivative",d).getEntry();
-    ffEntry = Shuffleboard.getTab("LaunchingTuning").add("feedForward",ff).getEntry();
-
+    m_follower.follow(m_motor,true);
+    m_motor.setIdleMode(IdleMode.kCoast);
+    m_follower.setIdleMode(IdleMode.kCoast);
+    
     Shuffleboard.getTab("LaunchingTuning").addNumber("pdp voltage", ()->Variables.getInstance().getPDPCurrent(3));
-    Shuffleboard.getTab("LaunchingTuning").addNumber("pv", encoder::getVelocity);
-    Shuffleboard.getTab("LaunchingTuning").addNumber("pv - rpm", ()->encoder.getVelocity()/encoder.getVelocityConversionFactor());
-    speedEntry = Shuffleboard.getTab("LaunchingTuning").add("speed",speed).getEntry();
-
-    m_setpointLimiter.setTarget(0);
-
-    initGains();
+    setTolerance(2);
   }
-
-  void initGains(){
-    pEntry.setDouble(0.03);
-    ffEntry.setDouble(0.005);
-    dEntry.setDouble(0.1);
-    updateGains();
-  }
-
   /**
    * Sets the speed of the Launcher wheel
    * @param speed the target speed of the wheel in inches per second
    */
   public void setSpeed(double newSpeed){
-    m_setpointLimiter.setTarget(newSpeed);
-    speed = newSpeed;
-    speedEntry.setDouble(speed);
+    setSetpoint(newSpeed);
   }
   public void changeSpeed(double angleDelta){
-    setSpeed(speed + angleDelta);
+    setSetpoint(m_mainController.getSetpoint()+angleDelta);
   }
 
   public boolean atSpeed(){
-    return Math.abs(speed - encoder.getVelocity()) <1;
+    return onTarget();
   }
   
   
@@ -88,11 +59,9 @@ public class TurretLauncher extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    updateGains();
-    double newSp = m_setpointLimiter.get();
-    controller.setReference(newSp, ControlType.kVelocity);
-    Variables.getInstance().setShooterEnabled((encoder.getVelocity() <=-Constants.minimumShootSpeed));
-    Variables.getInstance().setShooterAtSpeed(encoder.getVelocity()-speed<=2);
+    super.periodic();
+    Variables.getInstance().setShooterEnabled((m_processVariable.getAsDouble() <=-Constants.minimumShootSpeed));
+    Variables.getInstance().setShooterAtSpeed(atSpeed());
    
 
     double amps = Variables.getInstance().getPDPCurrent(3);
@@ -102,27 +71,5 @@ public class TurretLauncher extends SubsystemBase {
       Variables.getInstance().subtractPowerCell();
     }
     lastTrigger = trigger;
-  }
-  private void updateGains(){
-    if(p != pEntry.getDouble(0)){
-      p = pEntry.getDouble(0);
-      controller.setP(p);
-    }
-    if(i != iEntry.getDouble(0)){
-      i = iEntry.getDouble(0);
-      controller.setI(i);
-    }
-    if(d != dEntry.getDouble(0)){
-      d = dEntry.getDouble(0);
-      controller.setD(d);
-    }
-    if(ff != ffEntry.getDouble(0)){
-      ff = ffEntry.getDouble(0); 
-      controller.setFF(ff);
-    }
-    if(speed != speedEntry.getDouble(0)){
-      speed = speedEntry.getDouble(0); 
-      setSpeed(speed);
-    }
   }
 }
